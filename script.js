403f3c00c9b907259f6821b9eaa67dfbed47bf6d
shafidark <safi29317@gmail.com.com>
2020-07-22 04:42:49 +0600

    shafi

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Login Button even Start.
 let LoginBtn = document.getElementById("Login");
 LoginBtn.addEventListener('click', function () {
     let loginArea = document.getElementById("login-area");
@@ -5,3 +6,24 @@ LoginBtn.addEventListener('click', function () {
     let TransitionArea = document.getElementById('Transition-Area');
     TransitionArea.style.display = "block"
 });
+//Login Button even handler Finish.
+
+//Deposit Button even handler Start.
+let deposit = document.getElementById("addDeposit");
+deposit.addEventListener('click', function(){
+   let depositAmount = document.getElementById("deposit-Ammount").value;
+   let depositToNumber = parseFloat(depositAmount);
+   updateSpanText("currentDeposit", depositToNumber);
+   updateSpanText("currentBalence", depositToNumber);
+  document.getElementById("deposit-Ammount").value = "";
+});
+
+function updateSpanText(YourSpanId, depositToNumber){
+
+    let currentBalence = document.getElementById(YourSpanId).innerText;
+    let currentBalenceToNumber =  parseFloat(currentBalence);
+    let totalBalence = depositToNumber + currentBalenceToNumber;
+    document.getElementById(YourSpanId).innerText = totalBalence;
+}
+//Deposit Button even handler Finish.
+
